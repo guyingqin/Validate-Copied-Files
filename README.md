7abfcae9ffd90481035eb2a346856e5b3d9ef29d
Brijraj Singh <brijrajsingh@gmail.com>
2019-10-15 15:18:10 +0530

    Utilities updated for .net core

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-# Validate-Copied-Files
-After using [Azure Data Factory Copy Activity](https://docs.microsoft.com/en-in/azure/data-factory/copy-activity-overview) to copy the files (e.g. from S3 to Azure Blob), you can use this tool to verify all files are copied as expected.
+# Inventory audit tool (.NET Core based) for AWS S3 and Azure Blob store
+This tool is made up of 3 utilities which can be used seperately or together and can be useful to audit the files copied from AWS S3 to Azure Storage (Blob) and also returns the list of files which are not copied successfully from AWS S3 to Azure Storage and the log about what may be wrong with those files, in case of missing files one can always look at the log of both utilities and figure out which files may be missing. Based on .NET Core works for both Windows and Linux.
 
 ### Repository strcuture
   - /Source: includes the source codes.
-  - /Tool: includes the executable files to play with it directly.
+    - FetchBlobFileInfoCore - .net core (2.2) based app to download list of objects in a given Azure Storage Container.
+    - FetchS3FileInfoCore - .net core (2.2) based app to download list of objects in a given AWS S3 bucket
+    - ValidateCopiedFileLengthCore - Looks at the log of both above mentioned utilities, gives a count and also logs information about files missing in destination Azure storage container.
+  - /Tool: includes the executable files for both linux and Windows platforms and shell/batch scripts to run the tool.
+
+### .NET Core Installation
+Follow steps from here to install .NET Core on your machine https://dotnet.microsoft.com/download. For running the tool you may install the runtime only and for compiling the code opt for SDK installation. The tool is compiled to run on .NET Core 2.2.
+ 
---
+++ Source/FetchBlobFileInfoCore/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Newtonsoft.Json;
+using ValidateCopiedFileLengthCore;
+using System.Linq;
+
+namespace FetchBlobFileInfoCore
+{
+    class Program
+    {
+        static string connectionString = ConfigurationManager.AppSettings["connectionString"];
+        static string containerName = ConfigurationManager.AppSettings["containerName"];
+        static string path = ConfigurationManager.AppSettings["path"];
+        private static string specifiedContinuationTokenStr = ConfigurationManager.AppSettings["continuationToken"];
+
+        private const string OutputPath = Constants.FetchBlobFileInfo + "/" + Constants.OutputPath;
+        private const string LogPath = Constants.FetchBlobFileInfo + "/" + Constants.LogPath;
+
+        static void Main(string[] args)
+        {
+            ListAndRecordBlobSegments().Wait();
+        }
+
+
+        private static IEnumerable<FileItemInfo> ToFileItemInfos(IEnumerable<IListBlobItem> blobs, string basePath)
+        {
+            foreach (var blob in blobs)
+            {
+                var element = new FileItemInfo()
+                {
+                    Name = Helper.RemoveBasePath(blob.Uri.LocalPath, basePath)
+                };
+
+                ICloudBlob icloudBlob = blob as ICloudBlob;
+                if (icloudBlob != null)
+                {
+                    element.Length = icloudBlob.Properties.Length;
+                    element.LastModifiedUtc = icloudBlob.Properties.LastModified.Value.UtcDateTime;
+                }
+                else
+                {
+                    CloudBlob cloudBlob = blob as CloudBlob;
+                    if (cloudBlob != null)
+                    {
+                        element.Length = cloudBlob.Properties.Length;
+                        element.LastModifiedUtc = cloudBlob.Properties.LastModified.Value.UtcDateTime;
+                    }
+                }
+
+                if (element.Length != null)
+                    yield return element;
+            }
+        }
+
+        private static String ExtractMsg(Exception exception)
+        {
+            StorageException storageEx = exception as StorageException;
+
+            if (storageEx != null)
+            {
+                var message = storageEx.RequestInformation != null && storageEx.RequestInformation.ExtendedErrorInformation != null ? storageEx.RequestInformation.ExtendedErrorInformation.ErrorMessage : null;
+
+                if (message != null)
+                {
+                    // if this is a StorageClientException, log the extended error message which contains request id.
+                    return String.Format(CultureInfo.InvariantCulture, "{0}={1},", "StorageExtendedMessage", message);
+                }
+            }
+
+            return null;
+        }
+
+
+        private static async Task ListAndRecordBlobSegments()
+        {
+            var now = DateTime.Now.ToString("yyyy_MM_dd HH_mm_ss");
+            var outputFilePath = Helper.PathCombine(OutputPath, "Output_" + now + ".txt");
+            var logFilePath = Helper.PathCombine(LogPath, "Log_" + now + ".txt");
+
+            try
+            {
+
+                Helper.InitDirectory(OutputPath);
+                Helper.InitDirectory(LogPath);
+
+
+
+                BlobContinuationToken continuationToken = string.IsNullOrWhiteSpace(specifiedContinuationTokenStr)
+                    ? null
+                    : JsonConvert.DeserializeObject<BlobContinuationToken>(specifiedContinuationTokenStr);
+
+                var account = CloudStorageAccount.Parse(connectionString);
+                var client = account.CreateCloudBlobClient();
+
+                long fileCount = 0;
+                do
+                {
+
+                    var container = client.GetContainerReference(containerName);
+                    var directory = container.GetDirectoryReference(path);
+                    var blobResult = await directory.ListBlobsSegmentedAsync(true, BlobListingDetails.None, null, continuationToken, null, null);
+
+
+                    continuationToken = blobResult.ContinuationToken;
+                    var continuationTokenStr = continuationToken == null
+                        ? null
+                        : JsonConvert.SerializeObject(blobResult.ContinuationToken);
+                    var fileItemInfos = ToFileItemInfos(blobResult.Results, Helper.PathCombine(containerName, path)).ToList();
+
+                    fileCount += fileItemInfos.Count;
+
+                    File.AppendAllLines(outputFilePath, fileItemInfos.ToLines());
+                    File.AppendAllLines(logFilePath,
+                        new string[]
+                        {
+                            string.Format(CultureInfo.InvariantCulture,
+                                "Fetched Blob file count: {0}. Continuation token: '{1}'.", fileCount,
+                                continuationTokenStr)
+                        });
+                    Console.WriteLine("Fetched Blob file count: " + fileCount);
+
+                } while (continuationToken != null);
+
+                File.AppendAllLines(logFilePath,
+                    new string[] { "Fetch Blob file info completed. Total file count: " + fileCount });
+                Console.WriteLine("Fetch Blob file info completed. Total file count: " + fileCount);
+            }
+            catch (Exception e)
+            {
+                File.AppendAllLines(logFilePath,
+                    new string[] { "Fetch Blob file info failed: " + Helper.GetLogString(e, true, ExtractMsg) });
+                Console.WriteLine("Fetch Blob file info failed: " + Helper.GetLogString(e, true, ExtractMsg));
+                throw;
+            }
+        }
+    }
+}
+
